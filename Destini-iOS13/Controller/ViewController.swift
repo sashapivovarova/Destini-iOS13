43bd5ec2b1b3317eb3f6a8fc44cd7371d4a4f91a
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-04-17 23:26:29 +0900

    storyLabel の追加

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story0
     }
-
-
+    
+    func getTitle () {
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
 }
 
